1b612b5f41673bb8755f4685575a4fc613c08fff
Ru-Ri-z <ivangorojovsky91gmail.com>
2021-04-27 15:15:34 -0300

    completando about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,10 +7,7 @@ const About = () => {
       <div className="about-desc">
         <h3>Let me tell you something about me</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis
-          provident inventore quod voluptates sequi. Quis, necessitatibus omnis
-          iusto perferendis dolores voluptate et dolorum inventore autem nemo,
-          quos eveniet magni nesciunt.
+        I'm Ivan Gorojovsky. Since I was a kid I have been curious about computing, programming and technologies in general, but a few years ago I began to study  them formally. Fortunately, in develomment i found my vocation. By the way, I love preparing coffee while spending hours and hours creating, modeling and developing. I really enjoy the learning process, for this reason, I feel very identified with the quote "Live as if you were going to die tomorrow, learn as if you were going to live forever". In my free time i love spending it with my friends, updating myself on some technologies, reading romantic novelas and playng sports. My favourite ones are soccer and sport fishing. I consider myself to be a responsible, kind, firendly, optimistic and empathetic person. Also, I am keen on working with others and shearing ideas as a team from different perspectives to increase the knowledge on it.
         </p>
       </div>
       <div className="about-img">
